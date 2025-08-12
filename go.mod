2fff88b34678a072c7e4b14ab16a40ca383947b7
winkles99 <kiah.hurt@outlook.com>
2025-08-11 22:47:48 -0500

    chapter 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/winkles99/chirpy
+
+go 1.24.2
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
-
-import "fmt"
+import (
+    "net/http"
+)
 
 func main() {
-    fmt.Println("Hello, Chirpy!")
+    mux := http.NewServeMux()
+    //Readiness endpoint at .healthz
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+    })
+
+    fileServer := http.FileServer(http.Dir("."))
+    mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+
+    mux.Handle("/", fileServer)
+    server := http.Server{
+        Addr:    ":8080",
+        Handler: mux,
+    }
+server.ListenAndServe()
 }

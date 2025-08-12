4b8d472fb661e7d58c309f073303e1dc399ea69e
Winks <142744110+winkles99@users.noreply.github.com>
2025-08-12 22:27:54 +0000

    2.3

--- main.go
+++ main.go
@@ -1,60 +1,79 @@
 package main
+
 import (
-    "fmt"
-    "net/http"
-    "sync/atomic"
+	"fmt"
+	"net/http"
+	"sync/atomic"
 )
-//api configuration to manage file server metrics
+
+// api configuration to manage file server metrics
 type apiConfig struct {
-    fileserverHits atomic.Int32
+	fileserverHits atomic.Int32
 }
 
-//Middleware to increment hit counter
+// Middleware to increment hit counter
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        cfg.fileserverHits.Add(1) // thread-safe increment
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1) // thread-safe increment
+		next.ServeHTTP(w, r)
+	})
 }
 
-//Hit Shower
+// Hit Shower
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8") //Set Content Type
-    hits := cfg.fileserverHits.Load() //reads the current value
-    fmt.Fprintf(w, "Hits: %d", hits)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") //Set Content Type
+	hits := cfg.fileserverHits.Load()                           //reads the current value
+	fmt.Fprintf(w, "Hits: %d", hits)
 }
 
-//reset metrics
+// reset metrics
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-    cfg.fileserverHits.Store(0) //reset
-    w.WriteHeader(http.StatusOK)
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8") //set content type
-    w.Write([]byte("Hits reset to 0")) 
+	cfg.fileserverHits.Store(0) //reset
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") //set content type
+	w.Write([]byte("Hits reset to 0"))
 
 }
-
 
 func main() {
-    apiCfg := &apiConfig{}
-    mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+	mux := http.NewServeMux()
 
-    //file server
+	//file server
 	fsHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fsHandler))
-    
-    //Readiness endpoint at .healthz
-    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-        w.WriteHeader(http.StatusOK)
-        w.Write([]byte("OK"))
-    })
 
-    mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-    mux.HandleFunc("/reset", apiCfg.handlerReset)
+	//Readiness endpoint at .healthz
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodGet {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }    
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
-    server := http.Server{
-        Addr:    ":8080",
-        Handler: mux,
-    }
-server.ListenAndServe()
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		apiCfg.handlerMetrics(w, r)
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		apiCfg.handlerReset(w, r)
+
+	})
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
